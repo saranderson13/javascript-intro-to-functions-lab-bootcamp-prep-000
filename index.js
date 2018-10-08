5011e05a1dee6d93da2c5403aa9d75cd90da8c23
Sarah Anderson <saranderson13@gmail.com>
2018-10-08 16:05:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
-function logShout(string) {
-  var shout = string.toUpperCase;
-  
-  if(shout === string.toUpperCase) {
-    console.log('You did it!');
-  }
+function logShout() {
+  console.log('This is working.');
 }
